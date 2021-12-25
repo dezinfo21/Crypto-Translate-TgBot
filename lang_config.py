bace03cf8c36b6d3dbc6176475c5b25102585987
kirill <kirill2005wow@gmail.com>
2021-12-25 18:43:05 +0200

    All language configurations

---
+++ lang_config.py
@@ -0,0 +1,113 @@
+ADMIN_USERNAME = "@channel_support_team"
+ADMIN_LINK = "https://t.me/channel_support_team"
+
+
+WELCOME_LANGUAGES = {
+    'ru': 'Добро пожаловать. Этот бот поможет вам выбрать канал для получения нашей инфромации о криптовалюте на '
+          'удобном для вас языке. Что бы выбрать канал просто нажмите на необходимую кнопку внизу и вы автоматически '
+          'будете перенаправлены:',
+    'en': 'Welcome. This bot will help you choose a channel for receiving our information about cryptocurrencies in a '
+          'language convenient for you. To select a channel, just click on the required button below and you will be '
+          'automatically redirected:',
+    'ca': 'Benvingut. Aquest bot us ajudarà a triar un canal per rebre la nostra informació sobre criptomonedes en un '
+          'idioma que us convingui. Per seleccionar un canal, només cal que feu clic al botó requerit a continuació i '
+          'se us redirigirà automàticament:',
+    'fr': 'Bienvenue. Ce bot vous aidera à choisir un canal pour recevoir nos informations sur les crypto-monnaies '
+          'dans une langue qui vous convient. Pour sélectionner une chaîne, cliquez simplement sur le bouton requis '
+          'ci-dessous et vous serez automatiquement redirigé:',
+    'de': 'Willkommen zurück. Dieser Bot hilft Ihnen bei der Auswahl eines Kanals, um unsere Informationen über '
+          'Kryptowährungen in einer für Sie geeigneten Sprache zu erhalten. Um einen Kanal auszuwählen, '
+          'klicken Sie einfach unten auf die gewünschte Schaltfläche und Sie werden automatisch weitergeleitet:',
+    'ko': '환영하다. 이 봇은 귀하에게 편리한 언어로 암호화폐에 대한 정보를 수신할 채널을 선택하는 데 도움이 될 것입니다. 채널을 '
+          '선택하려면 아래에서 필요한 버튼을 클릭하기만 하면 자동으로 리디렉션됩니다:'
+}
+
+
+HELP_LANGUAGES = {
+    'ru': f'Что бы воспользоваться меню выбором канала отправьте команду /menu\n'
+          f'Если у вас возникнут вопросы вы можете задать их <a href="{ADMIN_LINK}">{ADMIN_USERNAME}</a>',
+    'en': f'To use the channel selection menu, send the command /menu\n'
+          f'If you have any questions, you can ask them to <a href="{ADMIN_LINK}">{ADMIN_USERNAME}</a>',
+    'ca': f'Per utilitzar el menú de selecció de canals, envieu l\'ordre /menu\n'
+          f'Si tens alguna pregunta, pots preguntar-les <a href="{ADMIN_LINK}">{ADMIN_USERNAME}</a>',
+    'fr': f'Pour utiliser le menu de sélection des chaînes, envoyez la commande /menun\n'
+          f'Si vous avez des questions, vous pouvez les poser <a href="{ADMIN_LINK}">{ADMIN_USERNAME}</a>',
+    'de': f'Um das Kanalauswahlmenü zu verwenden, senden Sie den Befehl /menu\n'
+          f'Wenn Sie Fragen haben, können Sie sie stellen <a href="{ADMIN_LINK}">{ADMIN_USERNAME}</a>',
+    'ko': f'채널 선택 메뉴를 사용하려면 명령을 보냅니다 /menu\n'
+          f'질문이 있는 경우 질문할 수 있습니다 <a href="{ADMIN_LINK}">{ADMIN_USERNAME}</a>'
+}
+
+
+ECHO_LANGUAGES = {
+    'ru': f'Не могу понять ваше сообщение! Пожалуйста, воспользуйтесь командой /help или обратитесь к '
+          f'<a href="{ADMIN_LINK}">{ADMIN_USERNAME}</a>',
+    'en': f'I can\'t understand your message! Please use the command or contact '
+          f'<a href="{ADMIN_LINK}">{ADMIN_USERNAME}</a>',
+    'ca': f'No entenc el teu missatge! Si us plau, fes servir l\'ordre /start o contacta amb '
+          f'<a href="{ADMIN_LINK}">{ADMIN_USERNAME}</a>',
+    'fr': f'Je ne comprends pas votre message ! Veuillez utiliser la commande /start ou contacter '
+          f'<a href="{ADMIN_LINK}">{ADMIN_USERNAME}</a>',
+    'de': f'Ich kann deine Nachricht nicht verstehen! Bitte benutze den /start Befehl oder kontaktiere deine '
+          f'<a href="{ADMIN_LINK}">{ADMIN_USERNAME}</a>',
+    'ko': f'나는 당신의 메시지를 이해할 수 없습니다! /start 명령어를 사용하거나 참조하시기 바랍니다 '
+          f'<a href="{ADMIN_LINK}">{ADMIN_USERNAME}</a>'
+}
+
+
+CHOOSE_CHANNEL_LANGUAGES = {
+    'ru': 'Выберете канал чтобы получать нашу информацию о криптовалюте на удобном вам языке:',
+    'en': 'Choose a channel to receive our information about cryptocurrency in your preferred language:',
+    'ca': 'Trieu un canal per rebre la nostra informació sobre criptomoneda en el vostre idioma preferit:',
+    'fr': 'Choisissez un canal pour recevoir nos informations sur la crypto-monnaie dans votre langue préférée:',
+    'de': 'Wählen Sie einen Kanal, um unsere Informationen zur Kryptowährung in Ihrer bevorzugten Sprache zu erhalten:',
+    'ko': '선호하는 언어로 암호화폐에 대한 정보를 수신할 채널을 선택하십시오.'
+}
+
+
+AVAILABLE_LANGUAGES = {
+    'en': {
+        'text': '🇺🇸 󠁧󠁢English(󠁧󠁢English)󠁧󠁢󠁥󠁮󠁧󠁿',
+        'link': 'https://t.me/crypto_signals_binance_pump'
+    },
+    'es': {
+        'text': '🇪🇸 España(Spain)',
+        'link': 'https://t.me/signals_pump_binance_espana'
+    },
+    'de': {
+        'text': '🇩🇪 Deutschland(Germany)',
+        'link': 'https://t.me/signals_pump_binance_deutschland'
+    },
+    'in': {
+        'text': '🇮🇳 हिन्दी(Hindi)',
+        'link': 'https://t.me/signals_pump_binance_india'
+    },
+    'ae': {
+        'text': '🇦🇪 عرب(Arab)',
+        'link': 'https://t.me/signals_pump_binance_oae'
+    },
+    'se': {
+        'text': '🇸🇪 Svenska(Swedish)',
+        'link': 'https://t.me/signals_pump_binance_sweden'
+    },
+    'no': {
+        'text': '🇳🇴 Norsk(Norwegian)',
+        'link': 'https://t.me/signals_pump_binance_norge'
+    },
+    'kr': {
+        'text': '🇰🇷 한국인(Korean)',
+        'link': 'https://t.me/signals_pump_binance_korea'
+    },
+    'cn': {
+        'text': '🇨🇳 中国人(Chinese)',
+        'link': 'https://t.me/signals_pump_binance_china'
+    },
+    'fr': {
+        'text': '🇫🇷 français(French)',
+        'link': 'https://t.me/signals_pump_binance_france'
+    },
+    'ru': {
+        'text': '🇷🇺 Русский(Russian)',
+        'link': 'https://t.me/pump_signals_binance_coins'
+    }
+}
